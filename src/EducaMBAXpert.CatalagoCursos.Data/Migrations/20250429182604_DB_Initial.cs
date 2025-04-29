8df60b19042deec5acac524b3c2015f8dfedc679
Marco Aurelio <marco@imperiumsolucoes.com.br>
2025-04-29 15:28:11 -0300

    Migrations

---
+++ src/EducaMBAXpert.CatalagoCursos.Data/Migrations/20250429182604_DB_Initial.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducaMBAXpert.CatalagoCursos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DB_Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Titulo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(100)", maxLength: 1000, nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Categoria = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nivel = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modulos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    CursoId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modulos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modulos_Cursos_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Cursos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aulas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Titulo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Url = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Duracao = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    ModuloId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aulas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aulas_Modulos_ModuloId",
+                        column: x => x.ModuloId,
+                        principalTable: "Modulos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aulas_ModuloId",
+                table: "Aulas",
+                column: "ModuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modulos_CursoId",
+                table: "Modulos",
+                column: "CursoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aulas");
+
+            migrationBuilder.DropTable(
+                name: "Modulos");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
